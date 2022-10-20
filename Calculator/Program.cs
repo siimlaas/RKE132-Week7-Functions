33f1c3f1b2ea4ff7e661494f237867bdf18e90c3
Siim Laas <laassiim@gmail.com>
2022-10-20 19:42:34 +0300

    add week 7 code

---
+++ Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection.PortableExecutable;
+
+Console.WriteLine("Enter the operation (+/-):");
+char userOperator = Char.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Enter a:");
+int firstNum = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Enter b:");
+int secondNum = Int32.Parse(Console.ReadLine());
+
+switch(userOperator)
+{
+    case '+':
+        Addition(firstNum, secondNum);
+        break;
+    case '-':
+        Subtraction(firstNum, secondNum);
+        break;
+    default:
+        Console.WriteLine("Wrong operator");
+        break;
+}
+
+static void Addition(int a, int b)
+{
+    Console.WriteLine($"{a} + {b} = {a + b}");
+}
+
+static void Subtraction(int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+}
